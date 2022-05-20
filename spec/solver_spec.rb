ffeecb7eae6145dba6e651a9d4f476da37a3fc3c
Omar Ragheb <ahmed.raph29@gmail.com>
2022-05-20 23:25:45 +0200

    fix test issue

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,28 +1,31 @@
 require './solver'
 
 describe Solver do
+    before(:each) do
+        @solver = Solver.new
+    end
     it 'Should factorial a number' do
-        expect(Solver.factorial(5)).to eq(120)
+        expect(@solver.factorial(5)).to eq(120)
     end
 
     it 'Should factorial a number' do
-        expect(Solver.factorial(10)).to eq(3628800)
+        expect(@solver.factorial(10)).to eq(3628800)
     end
 
     it 'Should reverse a string' do
-        expect(Solver.reverse("hello")).to eq("olleh")
+        expect(@solver.reverse("hello")).to eq("olleh")
     end
 
     it 'Should reverse My name' do
-        expect(Solver.reverse("Guy")).to eq("yuG")
+        expect(@solver.reverse("Guy")).to eq("yuG")
     end
 
     it 'Should fizzbuzz a number' do
-        expect(Solver.fizzbuzz(15)).to eq("FizzBuzz")
+        expect(@solver.fizzbuzz(15)).to eq("FizzBuzz")
     end
 
     it 'Should buzz a number' do
-        expect(Solver.fizzbuzz(5)).to eq("Buzz")
+        expect(@solver.fizzbuzz(5)).to eq("Buzz")
     end
 end
 
